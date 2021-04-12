6f250bf569f25ceb4200c86461658d807887bde2
AtomicLemon <enzogla@icloud.com>
2021-04-12 20:14:29 +1200

    Change Router address

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,7 @@ import { ChainId, JSBI, Percent, Token, WETH } from 'cloneswap-v2-sdk'
 
 import { fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 
-export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
+export const ROUTER_ADDRESS = '0xaDBeC0790d721A7b6C12d1ffD95066b9B46E2b16'
 
 // a list of tokens by chain
 type ChainTokenList = {
